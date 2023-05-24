1b0443804cb4a20074b97f148c8bee175b2c1bd3
NadeGolubeva <colombe466@gmail.com>
2023-05-24 22:43:28 +0300

    hw04im

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
-import {Toaster, toast} from 'react-hot-toast';
+import {
+  Toaster, toast} from 'react-hot-toast';
 
 import { Button } from "./Button/Button";
 import { ImageGallery } from "./ImageGallery/ImageGallery";
 import { Loader } from "./Loader/Loader";
 import { Modal } from "./Modal/Modal";
 import { Searchbar } from "./Searchbar/Searchbar";
-import { useState, useEffect } from 'react';
+import { Component } from 'react';
 import { findPic } from '../api/findPic'
 
 
@@ -21,6 +22,10 @@ export class App extends Component {
     empty: false,
 }
   
+// const handleChange = event => {
+//     setInputValue(event.target.value);
+//   };
+
   componentDidUpdate(prevProps, prevState) {
 const {search, page} = this.state
     if (
@@ -134,71 +139,3 @@ const {search, page} = this.state
   }
 };
 
-export const App = () => {
-  const [search, setSearch] = useState('');
-  const [images, setImages] = useState([]);
-  const [page, setPage] = useState(1);
-  const [total, setTotal] = useState(1);
-  const [isloading, setIsloading] = useState(false);
-  const [error, setError] = useState(null);
-  const [showModal, setShowModal] = useState(false);
-  const [empty, setEmpty] = useState(false);
-
-const loadingMore = () => {
-    setPage(prevState => ({
-       prevState + 1, 
-    }));
-};
-  
-// const openModal = (largeImageURL, alt) => {
-//     this.setState(({ showModal }) => {
-//       return { showModal: !showModal, largeImageURL, alt };
-//     });
-//   };
-
- const closeModal = () => {
-    setShowModal(({ showModal }) => {
-      
-      return { showModal: !showModal };
-    });
-  };
-
-   return (
-      <div>
-        <Toaster
-          position="top-center"
-          toastOptions={{
-            duration: 3000,
-     
-            style: {
-              fontSize: '28px',
-              // color: '#fff',
-            }
-          }}
-        />
-        <Searchbar handleSubmit={this.handleSubmit} />
-        {error && (
-          <h3>Something happed ({error}), please try again </h3>
-        )}
-
-
-        <ImageGallery controlModal={this.openModal} images={images} />
-        
-        {isLoading && <Loader  />}
-
-        {this.state.empty && (
-          <h3 style={{color: '#FF35ff', textAlign: 'center', fontSize: '36px'}}>We don't have such images</h3>
-        )}
-
-      
-       {total / 12 > page && <Button loadingMore={loadingMore} />}
-      
-        {this.state.showModal && (
-          <Modal closeModal={this.closeModal}>
-            <img src={this.state.largeImageURL} alt={this.state.alt} />
-          </Modal>
-        )}
-        
-      </div>
-    );
-}
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,39 +1,29 @@
 import PropTypes from 'prop-types'; 
 import css from './Searchbar.module.css';
-import { Component } from 'react';
+import { useState } from 'react';
 import { toast } from 'react-hot-toast';
 import { GrSearch } from "react-icons/gr";
 
-export class Searchbar extends Component {
-  state = {
-    search: '',
-  }
-  onChangeInput = (e) => {
-    const { name, value } = e.currentTarget; 
-    this.setState({ [name]: value });
-    // console.log(name, value);
-  }
 
-  // resetForm = () => {
-  //   this.setState({ search: '' });
-  // }
-  
-  render() {
-     return (
+
+
+export const Searchbar = ({handleSubmit}) => {
+  const [search, setSearch] = useState('');
+
+   return (
        <header
          className={css.searchbar}>
          <form
            className={css.searchForm}
            onSubmit={e => {
              e.preventDefault();
-             if (!this.state.search.trim()) {
+             if (!search.trim()) {
                return toast.error('Enter text to find pictures');
              }
-             this.props.handleSubmit(this.state.search);
-             const value = this.state.search;
+             handleSubmit(search);
+             const value = search;
              return value;
    
-             //  this.resetForm();
            }} >
            <button
              type="submit"
@@ -42,8 +32,8 @@ export class Searchbar extends Component {
            </button>
 
            <input
-           value={this.state.search}
-           onChange={this.onChangeInput}
+           value={search}
+           onChange={e => setSearch(e.target.value)}
            className={css.searchFormInput}
            name="search"
            type="text"
@@ -54,8 +44,7 @@ export class Searchbar extends Component {
   </form>
 </header>
     )
-  };
- }
+}
 
 Searchbar.propTypes = {
    handleSubmit: PropTypes.func.isRequired
